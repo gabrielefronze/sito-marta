5a643eda11fe1896b77ee036613732eae91a5793
Gabriele <sucre.91@hotmail.it>
2020-03-19 00:33:36 +0100

    Adding zenscroll as submodule

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 
       // Store hash
       var hash = this.hash;
+      alert(hash)
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
@@ -15,7 +16,7 @@ $(document).ready(function(){
       }, 800, function(){
   
         // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
+        window.location.hash = hash;s
       });
     } // End if
   });
