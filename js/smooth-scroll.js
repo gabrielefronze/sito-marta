350bb34745eabf03a882eb382e5fb68c2a0b734f
gabrielefronze <sucre.91@hotmail.it>
2020-03-18 15:30:39 +0100

    Anchors issue

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
-
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -9,7 +8,7 @@ $(document).ready(function(){
 
       // Store hash
       var hash = this.hash;
-
+      alert(hash)
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
