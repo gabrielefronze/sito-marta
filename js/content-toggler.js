5b9273c94394d6543aac4424592b98dabbfea7c0
gabrielefronze <sucre.91@hotmail.it>
2020-03-23 03:26:14 +0100

    Initializing variables onyl once

--- js/content-toggler.js
+++ js/content-toggler.js
@@ -7,7 +7,7 @@ var tophide;
 
 function toggleContent(id) {
   content_to_toggle = document.getElementById(id);
-  btn_to_toggle = document.getElementById(id+"btn")
+  btn_to_toggle = document.getElementById(id+"btn");
 
   for(var i = 0; i < contents.length; i++)
   {
@@ -19,25 +19,26 @@ function toggleContent(id) {
     btns[j].classList.remove("activebtn");
   }
 
-  content_to_toggle.classList.add("visible-content")
-  container.classList.remove("hide-container")
-  container.classList.add("shift-container")
-  tophide.classList.add("top-hide-visible")
-  btn_to_toggle.classList.add("activebtn")
+  content_to_toggle.classList.add("visible-content");
+  container.classList.remove("hide-container");
+  container.classList.add("shift-container");
+  tophide.classList.add("top-hide-visible");
+  if(btn_to_toggle) btn_to_toggle.classList.add("activebtn");
 
   setTimeout(function(){location.hash = id;}, 900);
 }
 
+function initContents() {
+  placeholder = document.getElementsByClassName("placeholder")[0];
+  container = document.getElementsByClassName("container")[0];
+  contents = document.getElementsByClassName("content");
+  btns = document.getElementsByClassName("dropbtn");
+  tophide = document.getElementsByClassName("top-hide")[0];
+}
+
 $(document).ready(
   function() {
-    var id = location.hash.replace('#','');
-
-    placeholder = document.getElementsByClassName("placeholder")[0];
-    container = document.getElementsByClassName("container")[0];
-    contents = document.getElementsByClassName("content");
-    btns = document.getElementsByClassName("dropbtn");
-    tophide = document.getElementsByClassName("top-hide")[0];
-    
+    var id = location.hash.replace('#','');    
     setTimeout(goTop(), 900);
     if(id!=="top") setTimeout(toggleContent(id), 900);
   }
--- js/return-top.js
+++ js/return-top.js
@@ -1,22 +1,16 @@
 function goTop() {
-  if (call === 0){
-    
-  }
-
   for(var j = 0; j < btns.length; j++)
   {
     btns[j].classList.remove("activebtn");
   }
 
-  tophide.classList.remove("top-hide-visible")
-  container.classList.remove("shift-container")
-  container.classList.add("hide-container")
+  tophide.classList.remove("top-hide-visible");
+  container.classList.remove("shift-container");
+  container.classList.add("hide-container");
 
   setTimeout(function(){location.hash = "top";}, 900);
 }
 
-$(document).ready(
-  function() {
-    top = document.getElementById("top");
-  }
-)
+function initTop() {
+  top = document.getElementById("top");
+}
