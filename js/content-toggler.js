2e6320542848e9251414ee265d4e5c65d2b55a3c
gabrielefronze <sucre.91@hotmail.it>
2020-03-24 01:47:29 +0100

    Fix page change animation

--- js/content-toggler.js
+++ js/content-toggler.js
@@ -5,34 +5,36 @@ var btns;
 var tophide;
 
 function toggleContent(id) {
-  content_to_toggle = document.getElementById(id);
-  btn_to_toggle = document.getElementById(id+"btn");
+  if(id !== window.hash){
+    content_to_toggle = document.getElementById(id);
+    btn_to_toggle = document.getElementById(id+"btn");
 
-  if(!placeholder) placeholder = document.getElementsByClassName("placeholder")[0];
-  if(!container) container = document.getElementsByClassName("container")[0];
-  if(!contents) contents = document.getElementsByClassName("content");
-  if(!btns) btns = document.getElementsByClassName("dropbtn");
-  if(!tophide) tophide = document.getElementsByClassName("top-hide")[0];
+    if(!placeholder) placeholder = document.getElementsByClassName("placeholder")[0];
+    if(!container) container = document.getElementsByClassName("container")[0];
+    if(!contents) contents = document.getElementsByClassName("content");
+    if(!btns) btns = document.getElementsByClassName("dropbtn");
+    if(!tophide) tophide = document.getElementsByClassName("top-hide")[0];
 
-  for(var i = 0; i < contents.length; i++)
-  {
-    if(contents[i]!==content_to_toggle) contents[i].classList.remove("visible-content");
-    else content_to_toggle.classList.add("visible-content");
+    for(var i = 0; i < contents.length; i++)
+    {
+      contents[i].classList.remove("visible-content");
+    }
+
+    window.scrollTo(0, 0);
+    content_to_toggle.classList.add("visible-content");
+
+    for(var j = 0; j < btns.length; j++)
+    {
+      if(btns[j]!==btn_to_toggle) btns[j].classList.remove("activebtn");
+      else btn_to_toggle.classList.add("activebtn");
+    }
+
+    container.classList.remove("hide-container");
+    container.classList.add("shift-container");
+    tophide.classList.add("top-hide-visible");
+
+    if(id!=='') history.pushState(null,null,'#'+id)
   }
-
-  for(var j = 0; j < btns.length; j++)
-  {
-    if(btns[j]!==btn_to_toggle) btns[j].classList.remove("activebtn");
-    else btn_to_toggle.classList.add("activebtn");
-  }
-
-  container.classList.remove("hide-container");
-  container.classList.add("shift-container");
-  tophide.classList.add("top-hide-visible");
-
-  if(id!=='') history.pushState(null,null,'#'+id)
-
-  // setTimeout(function(){location.hash = id;}, 900);
 }
 
 $(document).ready(
